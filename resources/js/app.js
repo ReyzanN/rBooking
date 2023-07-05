2785e48700d33711f1b1cce441fc9f479948973e
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-07-05 15:31:07 +0200

    Add JavaScript

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+import $ from "jquery";
+import 'datatables.net'
+
+/*
+DataTables Definitions
+*/
+$(document).ready(function(){
+    $('#MembersTable').DataTable({
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/fr-FR.json',
+        },
+        lengthMenu: [5,10,20,30,40,50]
+    });
+})
