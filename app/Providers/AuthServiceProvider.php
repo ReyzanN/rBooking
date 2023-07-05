9f0abc8543fe041d407191ee808e5a42fc49da30
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-07-05 15:31:33 +0200

    Update Provider for right

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,34 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        /*
+         * User Consultant Rank
+         */
+        Gate::define('UserConsultant',function(){
+            if (auth()->user()->IsConsultant()){
+                return  true;
+            }
+            return false;
+        });
+
+        /*
+         * User Admin Rank
+         */
+        Gate::define('UserAdmin',function(){
+            if (auth()->user()->IsAdmin()) {
+                return true;
+            }
+            return false;
+        });
+
+        /*
+         * User
+         */
+        Gate::define('UserSuperAdmin', function(){
+            if (auth()->user()->IsSuerAdmin()){
+                return true;
+            }
+           return false;
+        });
     }
 }
