441179d7bbd6c68b4294fc10fd7d8ce4f637577c
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-07-05 15:30:51 +0200

    Add toolbox

---
+++ app/Models/ToolBox.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+trait ToolBox
+{
+    /**
+     * @date 05/07/2023
+     * @param \DateTime|string $dateTime
+     * @return string
+     * @throws \Exception
+     */
+    public function ParseDateToString(\DateTime|string $dateTime):string {
+        if (is_string($dateTime)){
+            $Date = new \DateTime($dateTime);
+            return $Date->format('yd/m/y H:i');
+        }else {
+            return $dateTime->format('yd/m/y H:i');
+        }
+    }
+}
