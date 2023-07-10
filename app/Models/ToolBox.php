cf41ac48c712aed0ffa7d953eae3c81a256ce7eb
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-07-10 15:27:31 +0200

    Update toolbox

--- app/Models/ToolBox.php
+++ app/Models/ToolBox.php
@@ -18,4 +18,28 @@ trait ToolBox
             return $dateTime->format('d/m/y H:i');
         }
     }
+
+    public function ParseDateForAppointment(\DateTime|string $dateTime):string {
+        $Day = ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'];
+        $Month = array(
+            'Jan' => 'Janvier',
+            'Feb' => 'Février',
+            'Mar' => 'Mars',
+            'Apr' => 'Avril',
+            'May' => 'Mai',
+            'Jun' => 'Juin',
+            'Jul' => 'Juillet',
+            'Aug' => 'Août',
+            'Sep' => 'Septembre',
+            'Oct' => 'Octobre',
+            'Nov' => 'Novembre',
+            'Dec' => 'Décembre',
+        );
+        if (is_string($dateTime)){
+            $Date = new \DateTime($dateTime);
+            return $Day[$Date->format('w')].' '.$Date->format('d').' '.$Month[$Date->format('M')].' '.$Date->format('Y');
+        }else {
+            return $Day[$dateTime->format('w')].' '.$dateTime->format('d').' '.$Month[$dateTime->format('M')].' '.$dateTime->format('Y');
+        }
+    }
 }
