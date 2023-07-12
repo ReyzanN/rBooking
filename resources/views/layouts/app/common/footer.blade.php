a47b8ca2925a0062bf4df275e44114ac6ceaa52c
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-07-12 23:54:19 +0200

    Add footer

--- resources/views/layouts/app/common/footer.blade.php
+++ resources/views/layouts/app/common/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer mt-auto py-3 bg-body-tertiary">
-    <div class="container">
-        <span class="text-body-secondary">Place sticky footer content here.</span>
+<nav class="navbar fixed-bottom bg-body-tertiary rounded-3">
+    <div class="container-fluid d-flex justify-content-center align-items-center">
+        <p class="text-center opacity-75 text-tr" style="font-size: 0.75rem">&copy; 2023 Réalisation par <a href="https://www.reyzan.fr" target="_blank">@Gaetan</a></p>
     </div>
-</footer>
+</nav>
