5133dcc62eead6a36b8ad0a370fec3effe608d4a
Reyzan - Gaétan <g.sicardchamoret@laposte.net>
2023-07-17 13:40:47 +0200

    Update view

--- resources/views/customers/dashboard/index.blade.php
+++ resources/views/customers/dashboard/index.blade.php
@@ -15,27 +15,35 @@
                 <h4><i class="bi bi-calendar"></i>&nbsp;Rendez-vous de la journée</h4>
                 <span class="badge rounded-pill text-bg-light"><i class="bi bi-clock"></i>&nbsp; {{ $Time }}</span>
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-2">
-                    @foreach($AppointmentOfDay as $Registration)
-                        <div class="col">
-                            <div class="card shadow-sm">
-                                <div class="card-img-top d-flex justify-content-center align-items-center">
-                                    <i class="bi bi-calendar" style="font-size: 2rem"></i>&nbsp;{{ $Registration->GetAppointment()->GetAppointmentType()->name }}
-                                </div>
-                                <div class="card-body">
-                                    <p class="text-center"><i class="bi bi-calendar"></i>&nbsp;{{ $Registration->ParseDateToString($Registration->GetAppointment()->date) }}</p>
-                                    <div class="mt-2 mb-2 d-flex justify-content-center align-items-center">
-                                        <button class="btn btn-outline-primary"><i class="bi bi-eye"></i></button>
+                    @if(count($AppointmentOfDay) > 0)
+                        @foreach($AppointmentOfDay as $Registration)
+                            <div class="col">
+                                <div class="card shadow-sm">
+                                    <div class="card-img-top d-flex justify-content-center align-items-center">
+                                        <i class="bi bi-calendar" style="font-size: 2rem"></i>&nbsp;{{ $Registration->GetAppointment()->GetAppointmentType()->name }}
+                                    </div>
+                                    <div class="card-body">
+                                        <p class="text-center"><i class="bi bi-calendar"></i>&nbsp;{{ $Registration->ParseDateToString($Registration->GetAppointment()->date) }}</p>
+                                        <div class="mt-2 mb-2 d-flex justify-content-center align-items-center">
+                                            <button class="btn btn-outline-primary"><i class="bi bi-eye"></i></button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                        @endforeach
+                    @else
+                        <div class="d-flex justify-content-center align-items-center">
+                            <div class="alert alert-light text-center" role="alert">
+                                Aucun rendez-vous
+                            </div>
                         </div>
-                    @endforeach
+                    @endif
                 </div>
             </div>
             <div class="col">
                 <h4><i class="bi bi-calendar-check"></i>&nbsp;Rendez-vous en attente de confirmation</h4>
                 <div class="d-flex justify-content-center align-items-center">
-                    
+
                 </div>
             </div>
         </div>
